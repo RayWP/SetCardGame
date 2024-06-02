c49d49f43e5c30c99864caae045b1075bc30871f
RayWP <rwp1byte@gmail.com>
2024-06-02 17:21:14 +0800

    first commit

---
+++ app/src/main/java/com/r76127011/setcardgame/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.r76127011.setcardgame
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SetCardGame-R76127011"
+include(":app")
